619853ac8e4f92f668507d2a9ef1fb823295ddb7
Yiting <shenyiting2018@gmail.com>
2019-09-06 00:10:41 -0700

    add pre-commit

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,16 +3,17 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
-    
     def test_create_user_with_email_successfully(self):
         """Test creating a new user with an email is successfully"""
-        email = 'test@gmail.com'
-        password = 'testpass123'
+        email = "test@gmail.com"
+        password = "testpass123"
 
-        user = get_user_model().objects.create_user(
-            email=email,
-            password=password,
-        )
+        user = get_user_model().objects.create_user(email=email, password=password)
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        email = "test@GMAIL.COM"
+        user = get_user_model().objects.create_user(email, "test123")
+        self.assertEqual(user.email, email.lower())
