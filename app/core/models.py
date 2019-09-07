dd460c2722cc45e7ad3ee84b99960486aebc0caf
Yiting <shenyiting2018@gmail.com>
2019-09-06 21:55:54 -0700

    use black instead of autopep

--- app/core/models.py
+++ app/core/models.py
@@ -11,8 +11,19 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
+        if not email:
+            raise ValueError("Users must have email address")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None):
+        """Creates and saves a new superuser"""
+        user = self.create_user(email=self.normalize_email(email), password=password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -18,3 +18,17 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(email, "test123")
 
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """ Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "test123")
+
+    def test_create_new_super_user(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            "test@yitingworld.com", "test123"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
