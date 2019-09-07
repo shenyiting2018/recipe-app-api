07ebd78ee42bda7d3d5b0dedf57796db03bfee02
Yiting <shenyiting2018@gmail.com>
2019-09-06 21:34:48 -0700

    normalize email

--- app/core/models.py
+++ app/core/models.py
@@ -1,17 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-                                        PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
 # Create your models here.
-class UserManager(BaseUserManager):
 
+
+class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
 
@@ -22,4 +27,4 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -16,4 +16,5 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         email = "test@GMAIL.COM"
         user = get_user_model().objects.create_user(email, "test123")
+
         self.assertEqual(user.email, email.lower())
